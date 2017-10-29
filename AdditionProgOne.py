ea749dc26e3d1f2a5320590b0e3386df923706a3
NewtReno <ronnvelvet@hotmail.com>
2017-10-29 18:33:11 -0500

    AdditionProgOneV2.py
    I messed with it

--- AdditionProgOne.py
+++ AdditionProgOne.py
@@ -6,5 +6,5 @@ first_init = name_list[0][0]
 second_init = name_list[1][0]
 third_init = name_list[2][0]
 
-print('%s. %s. %s. ' % (first_init, second_init, third_init))
+print('%s.%s.%s.' % (first_init, second_init, third_init))
 
