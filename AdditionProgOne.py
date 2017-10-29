30e53bc806036c0ec4189a25d8a1c7bb97d11704
travc432 <travc432@gmail.com>
2017-10-29 18:28:01 -0500

    First commit

---
+++ AdditionProgOne.py
@@ -0,0 +1,10 @@
+#Travis Crotteau
+
+full_name = input("What is your full name?\n")
+name_list = full_name.split(" ")
+first_init = name_list[0][0]
+second_init = name_list[1][0]
+third_init = name_list[2][0]
+
+print('%s. %s. %s. ' % (first_init, second_init, third_init))
+
